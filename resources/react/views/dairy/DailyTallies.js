a15751502cb8129948392f607fa910424398d09b
ShreyasGijareTipic <shreyas.gijare@tipic.co.in>
2025-05-06 17:33:15 +0530

    DailyTally js changed

--- resources/react/views/dairy/DailyTallies.js
+++ resources/react/views/dairy/DailyTallies.js
@@ -491,7 +491,7 @@ function DailyProductLog() {
                                 {filteredCowFactory.map((item, index) => (
                                   <tr key={index}>
                                     <td>{item.product_name}</td>
-                                    <td>{item.quantity} packets</td>
+                                    <td>{item.quantity} {item.unit}</td>
                                   </tr>
                                 ))}
                               </tbody>
@@ -556,7 +556,7 @@ function DailyProductLog() {
                                 {filteredBuffaloFactory.map((item, index) => (
                                   <tr key={index}>
                                     <td>{item.product_name}</td>
-                                    <td>{item.quantity} packets</td>
+                                    <td>{item.quantity} {item.unit}</td>
                                   </tr>
                                 ))}
                               </tbody>
